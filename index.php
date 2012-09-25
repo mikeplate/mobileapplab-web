81d88f714fccd72f32a0db7e36d216c0cc7f8134
Mikael Plate <contact@mikeplate.com>
2012-09-25 12:18:07 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php
+ini_set('display_errors', '1');
+
+$site = yaml_parse_file('data/site.yaml');
+?>
+<html>
+    <body>
+        <h1><?php echo $site['title'] ?></h1>
+        <div class="now"><?php echo date('Y-m-d H:i:s'); ?></div>
+        <?php
+        # var_dump($site);
+
+        foreach ($site['menu'] as $chapter) {
+            echo '<h2>'.$chapter['title'].'</h2>';
+            if (isset($chapter['abstract']))
+                echo '<p>'.$chapter['abstract'].'</p>';
+
+            if (isset($chapter['menu'])) { 
+                foreach ($chapter['menu'] as $item) {
+                    echo '<h3>'.$item['title'].'</h3>';
+                    if (isset($item['description']))
+                        echo '<p>'.$item['description'].'</p>';
+
+                    if (isset($item['menu'])) {
+                        foreach ($item['menu'] as $subitem) {
+                            echo '<h4>'.$subitem['title'].'</h4>';
+                            if (isset($subitem['description']))
+                                echo '<p>'.$subitem['description'].'</p>';
+                        }
+                    }
+                }
+            }
+        }
+        ?>
+    </body>
+</html>
