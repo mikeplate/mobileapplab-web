43f7349467cc7d1e89568b7cc40e11a8d91efe97
Mikael Plate <contact@mikeplate.com>
2012-10-28 19:30:30 +0100

    Finalizing Design chapter

--- page.php
+++ page.php
@@ -1,4 +1,56 @@
 <!DOCTYPE html>
+<?php
+function output_menu($start, $start_url, $expand, $level) {
+    if (isset($start['menu'])) {
+        echo '<ul>';
+        foreach ($start['menu'] as $chapter) {
+            $url = $start_url . '/' . get_shortname($chapter);
+            $base_url = $url;
+            $has_children = isset($chapter['menu']) || isset($chapter['ref']) || isset($chapter['type']);
+            $type = 'page';
+
+            if (isset($chapter['type'])) {
+                $type = $chapter['type'];
+                $url .= '.' . $type;
+                echo '<li class="' . $type . '">';
+            }
+            else if ($has_children && !$expand) {
+                echo '<li class="menu">';
+            }
+            else if ($level>0) {
+                echo '<li class="submenu">';
+            }
+            else {
+                echo '<li>';
+            }
+
+            // Alternative types with extra link. Hardcoded to specific type(s) for now.
+            if ($type=='deck') {
+                $alttype = 'print';
+                if (isset($chapter['altname']))
+                    $altname = $chapter['altname'];
+                else
+                    $altname = $alttype;
+                echo '<a href="' . $base_url . '.' . $alttype . '" class="alttype ' . $alttype . '">' . $altname . '</a>';
+            }
+
+            if ($has_children && !$expand)
+                echo '<a href="'.$url.'">';
+            echo '<h2>'.$chapter['title'].'</h2>';
+            if (isset($chapter['description']))
+                echo '<p>' . get_html_for_text($chapter['description']) . '</p>';
+            if ($has_children && !$expand)
+                echo '</a>';
+
+            if ($expand)
+                output_menu($chapter, $start_url, $expand, $level+1);
+
+            echo '</li>';
+        }
+        echo '</ul>';
+    }
+}
+?>
 <html>
     <head>
         <title><?= $page_heading ?></title>
@@ -7,48 +59,6 @@
     </head>
     <body>
         <h1><?= $page_heading ?></h1>
-        <?php
-        if (isset($page['menu'])) {
-            echo '<ul>';
-            foreach ($page['menu'] as $chapter) {
-                $url = $page_url . '/' . get_shortname($chapter);
-                $base_url = $url;
-                $has_children = isset($chapter['menu']) || isset($chapter['ref']) || isset($chapter['type']);
-                $type = 'page';
-
-                if (isset($chapter['type'])) {
-                    $type = $chapter['type'];
-                    $url .= '.' . $type;
-                    echo '<li class="' . $type . '">';
-                }
-                else if ($has_children) {
-                    echo '<li class="menu">';
-                }
-                else {
-                    echo '<li>';
-                }
-
-                // Alternative types with extra link. Hardcoded to specific type(s) for now.
-                if ($type=='deck') {
-                    $alttype = 'print';
-                    if (isset($chapter['altname']))
-                        $altname = $chapter['altname'];
-                    else
-                        $altname = $alttype;
-                    echo '<a href="' . $base_url . '.' . $alttype . '" class="alttype ' . $alttype . '">' . $altname . '</a>';
-                }
-
-                if ($has_children)
-                    echo '<a href="'.$url.'">';
-                echo '<h2>'.$chapter['title'].'</h2>';
-                if (isset($chapter['description']))
-                    echo '<p>' . get_html_for_text($chapter['description']) . '</p>';
-                if ($has_children)
-                    echo '</a>';
-                echo '</li>';
-            }
-            echo '</ul>';
-        }
-        ?>
+        <?php output_menu($page, $page_url, isset($page['expand']), 0); ?>
     </body>
 </html>
