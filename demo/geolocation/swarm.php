76fbd95e11020fe9217fb3b26521a5c97a724a5b
Mikael Plate <contact@mikeplate.com>
2012-11-20 22:18:32 +0100

    Polished geolocation demo Swarm

--- demo/geolocation/swarm.php
+++ demo/geolocation/swarm.php
@@ -9,18 +9,46 @@ else {
     setcookie('swarm', $user_id);
 }
 
-if (file_exists('/.temp/swarm.json')) {
-    $jsondata = file_get_contents('/.temp/swarm.json');
-    $data = json_decode($jsondata);
+$data_file_path = $_SERVER['DOCUMENT_ROOT'] . '/.temp/swarm.json';
+if (file_exists($data_file_path) && !isset($_GET['reset'])) {
+    $jsondata = file_get_contents($data_file_path);
+    $data = json_decode($jsondata, true);
 }
 else {
-    $data = [];
+    $data = Array();
 }
 
-foreach ($obj in $data) {
+$current = null;
+for ($i = 0; $i < count($data); $i++) {
+    if ($data[$i]['id']==$user_id) {
+        $current = &$data[$i];
+    }
+    else if ((time()-strtotime($data[$i]['updated'])) > 864000) {
+        array_splice($data, $i, 1);
+        $i--;
+    }
 }
+if ($current==null) {
+    array_push($data, Array(
+        'id' => $user_id,
+        'latitude' => 0,
+        'longitude' => 0
+    ));
+    $current = &$data[count($data)-1];
+}
+$current['ip'] = $_SERVER['REMOTE_ADDR'];
+$current['useragent'] = $_SERVER['HTTP_USER_AGENT'];
+$current['updated'] = date('Y-m-d H:i:s');
+if (isset($_GET['latitude']))
+    $current['latitude'] = (double)$_GET['latitude'];
+if (isset($_GET['longitude']))
+    $current['longitude'] = (double)$_GET['longitude'];
 
 $jsondata = json_encode($data);
-file_put_contents('/.temp/swarm.json');
+file_put_contents($data_file_path, $jsondata);
+
+$current['self'] = true;
+$jsondata = json_encode($data);
+header('Content-type: application/json');
 echo $jsondata;
 ?>
