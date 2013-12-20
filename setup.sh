48b4b4ddeedefdf1e2d492518949a60144a851c4
Mikael Plate <contact@mikeplate.com>
2013-12-20 12:32:40 +0100

    Setup script updated

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install php5-curl
+PHP_INI=/etc/php5/fpm/php.ini
+sudo apt-get install php5-dev php-pear php5-curl libyaml-dev
+sudo pecl install yaml
+
+if [ ! -d lib ]; then
+    mkdir lib
+fi
+
+cd lib
+git clone --depth 1 https://github.com/imakewebthings/deck.js.git deck
 
